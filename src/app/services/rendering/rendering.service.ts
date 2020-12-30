73c2805aea536ad5ddbd731230c186eb07f4e306
michaellangbein <michael_langbein@hotmail.de>
2020-12-30 19:22:12 +0100

    using new model of mountain and new skybox

--- src/app/services/rendering/rendering.service.ts
+++ src/app/services/rendering/rendering.service.ts
@@ -2,10 +2,11 @@ import { Injectable, NgZone } from '@angular/core';
 import {
   AmbientLight, DirectionalLight, DoubleSide, Mesh, MeshBasicMaterial,
   PerspectiveCamera, PlaneGeometry, Scene, WebGLRenderer, MeshPhongMaterial,
-  AxesHelper, BoxGeometry, MeshLambertMaterial, Group, Vector3
+  AxesHelper, BoxGeometry, MeshLambertMaterial, Group, Vector3, TextureLoader, WebGLCubeRenderTarget, CubeTextureLoader
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { Collada, ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
 import { perlin3D } from './utils/noise';
 import { ArrayCube } from './utils/arrayMatrix';
 import { createMarchingCubeBlockMeshes, fetchWasm, MarchingCubeService } from './marchingCubes/marchingCubes';
@@ -32,45 +33,48 @@ export class RenderingService {
     renderer.setSize(container.clientWidth, container.clientHeight);
 
     const camera = new PerspectiveCamera(75, container.width / container.height, 0.1, 1000);
-    camera.position.z = 10;
-    camera.position.y = 4;
+    camera.position.z = 30;
+    camera.position.y = 20;
+    camera.position.x = -20;
     camera.name = 'camera';
 
-    const light = new DirectionalLight('white', 0.6);
+    const light = new DirectionalLight('white', 0.8);
     light.position.x = -3;
     light.position.y = 1;
     light.position.z = 5;
     light.name = 'light1';
     scene.add(light);
 
-    const light2 = new AmbientLight('#c5f8f3', 0.2);
+    const light2 = new AmbientLight('#c5f8f3', 0.3);
     light2.name = 'light2';
     scene.add(light2);
 
-    const axesHelper = new AxesHelper(5);
-    axesHelper.name = 'axesHelper';
-    scene.add(axesHelper);
-
-    const skyBox = new Mesh(
-      new BoxGeometry(500, 500, 500, 3, 3, 3),
-      new MeshLambertMaterial({
-        color: '#daf8e3',
-        side: DoubleSide
-      })
-    );
-    skyBox.name = 'skybox';
-    scene.add(skyBox);
-
     const controls = new OrbitControls(camera, container);
 
-    const loader = new OBJLoader();
-    loader.load('assets/mtStHelens.obj', (group: Group) => {
-      const model = group.children[0];
+    const helpers = new AxesHelper(10);
+    scene.add(helpers);
+
+    const textureLoader = new CubeTextureLoader();
+    const texture = textureLoader.load([
+      'assets/skybox/Daylight_Box_Right.bmp',
+      'assets/skybox/Daylight_Box_Left.bmp',
+      'assets/skybox/Daylight_Box_Top.bmp',
+      'assets/skybox/Daylight_Box_Bottom.bmp',
+      'assets/skybox/Daylight_Box_Front.bmp',
+      'assets/skybox/Daylight_Box_Back.bmp',
+    ]);
+    scene.background = texture;
+
+    const loader = new ColladaLoader();
+    loader.load('assets/mtStHelensCollada/model.dae', (data: Collada) => {
+      const model = data.scene.children[0].children[1];
       model.name = "MtStHelens";
-      model.scale.set( 0.001, 0.001, 0.001);
+      model.scale.set( 0.2, 0.2, 0.2);
+      model.position.add(new Vector3(-70, 0, -80));
       model.rotateX(- Math.PI / 2);
+      model.rotateZ(Math.PI);
       scene.add(model);
-      console.log(model);
+      console.log(model)
     });
 
     this.scene = scene;
